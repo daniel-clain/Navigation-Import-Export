47b733956219047d9b0bedf1e51b5d93a5f8768d
daniel-clain <daniel.clain@hotmail.com>
2021-02-14 13:42:28 +1100

    update cache

--- src/setMenu/setMenu-implementation/getMenuData.ts
+++ src/setMenu/setMenu-implementation/getMenuData.ts
@@ -4,7 +4,7 @@ import state from '../../state'
 
 export default async () => {
   console.log('* getting menu data from cache')
-  const fileToWriteTo = "menuData.cache.json"
+  const fileToWriteTo = "cache/menuData.cache.json"
   try{
     const fileData = await fsPromises.readFile(fileToWriteTo)
     state.menuData = JSON.parse(fileData.toString())
