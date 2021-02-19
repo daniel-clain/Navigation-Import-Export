1d768af2fabfe2e183e88194c8f52127bf16baef
daniel-clain <daniel.clain@hotmail.com>
2021-02-20 10:25:56 +1100

    wip

---
+++ config/inputVariables.data.ts
@@ -0,0 +1,19 @@
+import {partnersLoginPassword} from './passwords/passwords'
+
+export const inputVariables = {
+  menuName: '2020 - Main Menu',
+  fromStore: {
+    storeName: 'Kirstin Ash (Australia)', 
+    storeUrl: ''
+  },
+  toStore: {
+    storeName: 'Kirstin Ash (Europe)',
+    storeUrl: ''
+  },  
+  automateLogin:{
+    active: false,
+    partnersLoginEmail: 'daniel@processcreative.com.au',
+    partnersLoginPassword,
+  },
+  showInBrowser: true,
+}
